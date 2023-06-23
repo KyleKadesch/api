a371ebd52d6fe2b425b5b9627a0142f2ef4de7c6
Kyle Kadesch <kyleak511@gmail.com>
2023-06-23 11:21:59 -0500

    Resetting changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = express.Router();
 
 const app = express();
 const port = 4041;
@@ -8,10 +7,30 @@ const port = 4041;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use('/', require('./routes/users'));
+const users = [{name: 'Tony', email: 'tony@mail.com'}]
+
+app.get('/', (_, res) => {
+    res.send('wooooo');
+});
+
+app.get('/users', (_, res) => {
+    res.json({ok: true, users});
+});
+
+app.get('/user/:name', (req, res) => {
+    const {name} =req.params;
+    const user = users.filter((user) => user.name === name)[0];
+    res.json({ok: true, user});
+});
+
+app.post('/adduser', (req, res) => {
+    const {name, email} = req.body;
+        if(name && email) {
+            users.push({name, email});
+            res.json({ok: true, users});
+        }
+});
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
 });
-
-module.exports = router;
